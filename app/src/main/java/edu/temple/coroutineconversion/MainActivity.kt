7c612b18a2e4d4d9bf0bd88b5d3ab0e0595fd9a4
thegreatandmightywafflecaptain <sirmadametheknight@gmail.com>
2024-03-27 15:26:32 -0400

    Added CoroutineScope and used scope.launch when button is clicked

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -6,6 +6,10 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,13 +28,17 @@ class MainActivity : AppCompatActivity() {
 
         cakeImageView = findViewById(R.id.imageView)
 
+        val scope = CoroutineScope(Job() + Dispatchers.Default)
+
         findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
-                }
-            }.start()
+            scope.launch{
+                Thread{
+                    repeat(100) {
+                        handler.sendEmptyMessage(it)
+                        Thread.sleep(40)
+                    }
+                }.start()
+            }
         }
     }
 }
